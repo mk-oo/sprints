c0552887e7268573326b69cd5e0143447cbaf7d8
mk-oo <48906668+mk-oo@users.noreply.github.com>
2021-09-05 02:14:31 +0200

    Update original.js

--- Chat-App-Pusher/original.js
+++ Chat-App-Pusher/original.js
@@ -1,57 +1,20 @@
 var firstScreen = document.querySelector('#first-screen');
 var scecondScreen = document.querySelector('#second-screen');
 let msgBtn = document.querySelector('#sendmsg-btn');
-var group = document.querySelector('#group-name-inp').value;
 var user = 0.1;
 var userEntriesObjects = {};
 var userEntriesArray = [];
-//userEntriesArray.push(userEntriesObjects);
-//localStorage.setItem('key', JSON.stringify(userEntriesArray));
 var event = 'my-event'
 var room = 'my-channel'
 var name = "";
 
 
-// function startTimer(duration, display) {
-//     var timer = duration,
-//         minutes, seconds;
-//     setInterval(function () {
-//         minutes = parseInt(timer / 60, 10);
-//         seconds = parseInt(timer % 60, 10);
-
-//         seconds = seconds < 10 ? "0" + seconds : seconds;
-
-//         display.textContent = seconds;
-
-//         if (--timer < 0) {
-//             //clearInterval(timer);
-//             //display.textContent ="00:00";
-
-//             logout();
-//         }
-
-//     }, 1000);
-// }
-
-// function tIMER() {
-//     var oneminute = 60 * 1,
-//         display = document.querySelector('#time');
-
-//     startTimer(oneminute, display);
-
-
-// };
-
-
-
-
 function join() {
-    var groupInp = document.querySelector('#group-name-inp').value;
-    var nameInp = document.querySelector('#name-inp').value;
-    let group = validategroupName(groupInp);
-    let name = validateName(nameInp);
 
-    console.log(group);
+    room = validategroupName(document.querySelector('#group-name-inp').value);
+    name = validateName(document.querySelector('#name-inp').value);
+
+    console.log(room);
     console.log(name);
     user = getGUID();
 
@@ -61,12 +24,16 @@ function join() {
         firstScreen.style.display = 'none';
         scecondScreen.style.display = "block";
     }
-    
-    storeLocalStrorage(name,user,group);
-    subscribeToPusher(groupInp, user);
-    tIMER();
+    timer(60);
+    storeLocalStrorage(name, user, room);
+    subscribeToPusher(room, user);
+
+
 }
-function storeLocalStrorage(nameOfUser,idOfUser,group) {
+
+
+
+function storeLocalStrorage(nameOfUser, idOfUser, room) {
     let div = document.querySelector('#online-users');
 
 
@@ -88,8 +55,8 @@ function storeLocalStrorage(nameOfUser,idOfUser,group) {
     onlineMembers = JSON.parse(localStorage.getItem('key')).length;
     console.log(onlineMembers);
 
-    div.innerHTML = `${group}: Online Users (${onlineMembers}) `;
-    
+    div.innerHTML = `${room}: Online Users (${onlineMembers}) `;
+
 }
 
 let subscribeToPusher = async function (room, user) {
@@ -103,50 +70,26 @@ let subscribeToPusher = async function (room, user) {
 }
 
 
-function startTimer(duration, display) {
-    var timer = duration,
-        minutes, seconds;
-    setInterval(function () {
-        minutes = parseInt(timer / 60, 10);
-        seconds = parseInt(timer % 60, 10);
-
-        seconds = seconds < 10 ? "0" + seconds : seconds;
-
-        display.textContent = seconds;
-
-        if (--timer < 0) {
-
-            logout();
-        }
-    }, 1000);
-}
-
-function tIMER() {
-    var oneminute = 60 * 1;
-    display = document.querySelector('#time');
-    startTimer(oneminute, display);
-};
-
-
-
-
 recievedMesage = async function (channel, user) {
     channel.bind('my-event', function (data) {
         respond = JSON.parse(JSON.stringify(data));
         message = respond.message;
         senderId = respond.userId;
+        senderName = respond.userName;
         addedMsg = document.querySelector('#chat');
 
         if (user == senderId) {
             addedMsg.insertAdjacentHTML("beforeend", `<p id= 'sender'> You: ${message}</p>`)
         }
         if (user != senderId) {
-            addedMsg.insertAdjacentHTML("beforeend", `<p id ='reciever'>User: ${message}</p>`)
+            addedMsg.insertAdjacentHTML("beforeend", `<p id ='reciever'>${senderName}: ${message}</p>`)
         }
 
 
     });
 }
+
+
 async function sendMessage() {
     console.log('Send Button Clicked')
     message = document.querySelector('#sendmsg-inp').value;
@@ -156,13 +99,15 @@ async function sendMessage() {
         alert('your text area is empty');
         return;
     }
+    document.querySelector('#time').innerText = 60
+
     pusherAPI(message);
 }
 
 async function pusherAPI(message) {
 
     let body = {
-        data: '{"message":"' + message + '","userName":"' + name + '","userId":"' + user + '"}',
+        data: `{"message":"${message}","userName":"${name}","userId":"${user}"}`,
         name: "my-event",
         channel: room
     }
@@ -176,7 +121,9 @@ async function pusherAPI(message) {
             'Content-Type': 'application/json'
         }
     });
+
 }
+
 function validateName(name) {
     if (name === null || name.length === 0 || name.replace(/^\s+|\s+$/gm, '') == "" || Boolean(name) === false) {
         alert('enter your name');
@@ -185,7 +132,6 @@ function validateName(name) {
         return name;
     }
 }
-
 function validategroupName(groupNam) {
     if (groupNam === null || groupNam.length === 0 || groupNam.replace(/^\s+|\s+$/gm, '') == "" || Boolean(groupNam) === false) {
         alert('enter your group name');
@@ -194,15 +140,12 @@ function validategroupName(groupNam) {
         return groupNam;
     }
 }
-
 function getMD5(body) {
     return CryptoJS.MD5(JSON.stringify(body));
 }
-
 function getAuthSignature(md5, timeStamp) {
     return CryptoJS.HmacSHA256(`POST\n/apps/1258784/events\nauth_key=b2d0db28cededf04656d&auth_timestamp=${timeStamp}&auth_version=1.0&body_md5=${md5}`, "c83118ee1cad8cf369f7");
 }
-
 function getGUID() {
     return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
         var r = (Math.random() * 16) | 0,
@@ -210,13 +153,11 @@ function getGUID() {
         return v.toString(16);
     });
 }
-
 function getTime() {
     var today = new Date();
     var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
     return time
 }
-
 function logout() {
     firstScreen.style.display = 'block';
     scecondScreen.style.display = "none";
@@ -228,3 +169,18 @@ document.addEventListener('keypress', function (event) {
         sendMessage();
     }
 })
+function timer(time) {
+    time = Number(document.querySelector('#time').innerText);
+    var timer = setInterval(function () {
+        if (time > 1) {
+            time = Number(document.querySelector('#time').innerText);
+            display = document.querySelector('#time');
+            time--;
+            display.textContent = time;
+        } else {
+            logout();
+            clearInterval(interval)
+        }
+    }, 1000);
+
+}
